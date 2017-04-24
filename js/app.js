14e7aa1847b2c9e43a344ca3b8887b507167b3f6
Tonya <tonya.scorpio@gmail.com>
2017-04-24 16:38:31 +0300

    Add remote data loading

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = new Vue({
     data: {
         selectedCity: 'Бухарест',
         parts: [{
-            name: 'День',
+            name: 'Ранок',
             hours: [{
                 time: 14,
                 temperature: -2,
@@ -19,6 +19,32 @@ var app = new Vue({
                 windSpeed: 5,
                 probability: 0.2
             }]
-        }]
+        }], 
+        days: []
+    },
+    computed: {
+    	daysCount: function() {
+    		return this.days.length;
+    	}
+    },
+    methods: {
+        forecast: function() {
+        	// Save a link to the current component root
+        	var component = this;
+        	// Load a remote data
+            fetch('https://api.apixu.com/v1/forecast.json?key=f795a251e1174012b7a125426171604&q=Bucharest&days=7')
+                .then(function(response) {
+                	// Transform a loaded data into the JSON
+                    return response.json();
+                })
+                .then(function(data) {
+                	// Assign loaded and transormed data into the component property                	
+                    component.days = data.forecast.forecastday;
+                    console.log(component.days);
+                });
+        }
+    }, mounted: function() {
+    	// This will be executed when our component is fully ready to work
+    	this.forecast();
     }
 });
